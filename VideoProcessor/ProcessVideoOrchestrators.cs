fbeed6e6d1b2314a500982ea1257797eb012355b
Mark Heath <mark.heath@gmail.com>
2018-04-23 18:38:03 +0100

    created empty orchestrator function, end of M2 C6

---
+++ VideoProcessor/ProcessVideoOrchestrators.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoProcessor
+{
+    public static class ProcessVideoOrchestrators
+    {
+        [FunctionName("O_ProcessVideo")]
+        public static async Task<object> ProcessVideo(
+            [OrchestrationTrigger] DurableOrchestrationContext ctx,
+            TraceWriter log)
+        {
+
+
+        }
+    }
+}
