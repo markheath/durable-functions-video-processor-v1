d643472236013957852411895336af53aad4b002
Mark Heath <mark.heath@gmail.com>
2018-04-24 20:25:09 +0100

    extra logging for orchestrator demo

--- VideoProcessor/ProcessVideoOrchestrators.cs
+++ VideoProcessor/ProcessVideoOrchestrators.cs
@@ -13,11 +13,20 @@ namespace VideoProcessor
         {
             var videoLocation = ctx.GetInput<string>();
 
+            if (!ctx.IsReplaying)
+                log.Info("About to call transcode video activity");
+
             var transcodedLocation = await
                 ctx.CallActivityAsync<string>("A_TranscodeVideo", videoLocation);
 
+            if (!ctx.IsReplaying)
+                log.Info("About to call extract thumbnail");
+
             var thumbnailLocation = await
                 ctx.CallActivityAsync<string>("A_ExtractThumbnail", transcodedLocation);
+
+            if (!ctx.IsReplaying)
+                log.Info("About to call prepend intro");
 
             var withIntroLocation = await
                 ctx.CallActivityAsync<string>("A_PrependIntro", transcodedLocation);
